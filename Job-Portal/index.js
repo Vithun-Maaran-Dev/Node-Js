ba5b28f127aa5c586a03b82e8068f1b511a4af65
Vithun Maaran <vithunmaaran@gmail.com>
2025-03-05 23:33:15 +0530

    Added new pages.

--- Job-Portal/index.js
+++ Job-Portal/index.js
@@ -2,17 +2,34 @@ import express from "express"
 import path from "path"
 import ejsLayout from "express-ejs-layouts"
 
+//import controller
+import { loginView, registerView } from "./src/controllers/index.controller.js";
+
 //creating server
 const server = express();
 
 server.set('view engine', 'ejs');
 server.set('views', path.resolve(`src`, `views`))
 server.use(ejsLayout);
+server.use(express.static('public'));
+
 
 
 server.get(`/`, (req, res) => {
      res.render("index")
 });
+
+//index view
+server.get(`/login`, loginView);
+server.get(`/register`, registerView);
+
+
+
+
+
+
+
+
 
 
 //creating port
