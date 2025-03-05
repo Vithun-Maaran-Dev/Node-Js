44e7530f0480ddf8cbbaf97bfab9ec4566b8f3be
Vithun Maaran <vithunmaaran@gmail.com>
2025-03-05 11:54:48 +0530

    Added new project

---
+++ Job-Portal/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import path from "path"
+
+//creating server
+const server = express();
+
+server.set('view engine', 'ejs');
+server.set('views', path.resolve(`src`, `views`))
+
+
+server.get(`/`, (req, res) => {
+
+})
+
+
+//creating port
+server.listen(5000, (err) => {
+     if (err) {
+          console.log(err)
+     }
+     else {
+          console.log(`server is running...`);
+     }
+})
