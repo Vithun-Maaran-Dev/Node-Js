01f78f13dc899cef8c53645604e5c7fb27e40f97
Vithun Maaran <vithunmaaran@gmail.com>
2025-03-13 21:41:39 +0530

    Done the project logic

--- Job-Portal/src/controllers/admin.controller.js
+++ Job-Portal/src/controllers/admin.controller.js
@@ -1,10 +1,11 @@
 import { getJobsWithRecuriterName } from "../models/admin.model.js";
+import { getAllUsers } from "../models/users.model.js";
 
 export const adminDashboardView = (req, res) => {
      return res.render('adminDashboard');
 }
 
-export const getAllJobs = (req, res) => {
+export const getAdminAllJobs = (req, res) => {
 
      const jobs = getJobsWithRecuriterName();
 
@@ -14,5 +15,17 @@ export const getAllJobs = (req, res) => {
      else {
           return res.status(400).render('adminJobsView', { found: jobs.success, mess: jobs.message })
      }
-
 }
+
+export const getAdminAllUsers = (req, res) => {
+
+     const users = getAllUsers();
+
+     if (users.length > 0) {
+          const userAndRecuriterOnly = users.filter(user => user.role === 'J' || user.role === 'R')
+          res.status(200).render('adminUsersView', { users: userAndRecuriterOnly });
+     }
+     else {
+          res.status(400).render(('adminUsersView', { users: [] }))
+     }
+} 
