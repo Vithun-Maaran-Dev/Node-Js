15e74d34d35ed078063fe2503363aac37156abba
Vithun Maaran <vithunmaaran@gmail.com>
2025-03-14 19:21:23 +0530

    Added newe project social media

---
+++ Social-Media/index.js
@@ -0,0 +1,37 @@
+import express from "express"
+import path from "path"
+import ejsLayout from "express-ejs-layouts"
+import session from "express-session";
+
+
+
+//creating server
+const server = express();
+
+server.set('view engine', 'ejs');
+server.set('views', path.resolve(`src`, `views`))
+server.use(ejsLayout);
+server.use(express.static('public'));
+
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }));
+
+
+server.get(`/`, (req, res) => {
+     res.render("index")
+});
+
+
+
+
+
+
+//creating port
+server.listen(5000, (err) => {
+     if (err) {
+          console.log(err)
+     }
+     else {
+          console.log(`server is running...`);
+     }
+})
